10dc0ae58c0952e5bd54931329363ac3775bbc0c
David Zegarra <87683424+zrDavid@users.noreply.github.com>
2024-02-12 14:41:51 -0500

    added last name input

--- basicPrint.c
+++ basicPrint.c
@@ -7,10 +7,12 @@ void main()
 	// Welcome message
 	printf("Welcome to the test program!\n");
 	printf("Enter your name: ");
-	scanf("%s", name);
+	scanf("%s", fname);
+	printf("Enter your last name: ");
+	scanf("%s", lname);
 	printf("-------------\t-------------\t-------------\t\n");
-	printf("Hello %s\n\n", name);
+	printf("Hello %s %s\n\n", fname, lname);
 	printf("What is your age?: ");
 	scanf("%d", &age);
-	printf("Hey %s, you are young, you are just %d years old", name, age);
+	printf("Hey %s, you are young, you are just %d years old", fname, age);
 }

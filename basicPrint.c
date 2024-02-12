4b0da2c9909467b716e6cc47efcc26435f23430e
David Zegarra <87683424+zrDavid@users.noreply.github.com>
2024-02-12 14:16:47 -0500

    Added Welcome Message

--- basicPrint.c
+++ basicPrint.c
@@ -3,6 +3,9 @@
 void main()
 {
 	char name[20];
+	int age;
+	// Welcome message
+	printf("Welcome to the test program!\n");
 	printf("Enter your name: ");
 	scanf("%s", name);
 	printf("-------------\t-------------\t-------------\t\n");
